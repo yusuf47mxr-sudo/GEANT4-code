e9fd805b6fe17fa199608a4e70072a1ee8f73c31
Yusuf <yusuf47mxr@gmail.com>
2026-05-13 18:42:30 +0300

    TANK_YENUU guncalandu

--- TANK_YENUU/src/TankDetectorConstruction.cc
+++ TANK_YENUU/src/TankDetectorConstruction.cc
@@ -7,41 +7,58 @@
 #include "G4SystemOfUnits.hh"
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
+#include "G4OpticalSurface.hh"      
+#include "G4LogicalSkinSurface.hh"  
 
 TankDetectorConstruction::TankDetectorConstruction() {}
 TankDetectorConstruction::~TankDetectorConstruction() {}
 
 G4VPhysicalVolume* TankDetectorConstruction::Construct() {
-    // Malzeme Yöneticisi
     G4NistManager* nist = G4NistManager::Instance();
+    
     G4Material* env_air = nist->FindOrBuildMaterial("G4_AIR");
     G4Material* water   = nist->FindOrBuildMaterial("G4_WATER");
 
-    // --- ÇERENKOV İÇİN EN KRİTİK NOKTA: SUYUN KIRMA İNDİSİ ---
-    // Eğer bunu yapmazsan su ışığı kırmaz, mavi fotonlar oluşmaz!
-    G4double photonEnergy[] = {2.0*eV, 3.4*eV}; // Görünür ışık aralığı
-    G4double waterRINDEX[] = {1.33, 1.33};      // Suyun kırma indisi
+    // SADECE SUYUN KIRMA İNDİSİ
+    G4double photonEnergy[] = {1.5*eV, 3.4*eV};
+    G4double waterRINDEX[]  = {1.33, 1.33}; 
     
     G4MaterialPropertiesTable* mptWater = new G4MaterialPropertiesTable();
     mptWater->AddProperty("RINDEX", photonEnergy, waterRINDEX, 2);
-    water->SetMaterialPropertiesTable(mptWater); // Özelliği suya ekle
+    water->SetMaterialPropertiesTable(mptWater);
 
-    // 1. DÜNYA (WORLD) - GÖRÜNMEZ YAPIYOM
+    // DÜNYA VE TANK
     G4Box* solidWorld = new G4Box("World", 1.5*m, 1.5*m, 1.5*m);
     G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, env_air, "World");
-    logicWorld->SetVisAttributes(G4VisAttributes::GetInvisible()); // Dünya görünmez olsun ki kalabalık yapmasın
+    logicWorld->SetVisAttributes(G4VisAttributes::GetInvisible()); 
     G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "World", 0, false, 0, true);
 
-    // 2. SU TANKI - İÇİ DOLU SU
-    // Yarıçap: 52.5 cm, Yarı-boy: 60 cm
     G4Tubs* solidTank = new G4Tubs("SuTanki", 0.*cm, 52.5*cm, 60.0*cm, 0.*deg, 360.*deg);
     G4LogicalVolume* logicTank = new G4LogicalVolume(solidTank, water, "SuTanki");
     new G4PVPlacement(0, G4ThreeVector(0,0,0), logicTank, "SuTanki", logicWorld, false, 0, true);
 
-    // --- GÖRSELLİK: TANKI YEŞİL TEL ÖRGÜ YAP! ---
-    G4VisAttributes* tankVis = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0)); // Yeşil renk
-    tankVis->SetForceWireframe(true); // İçi boş kafes (tel örgü) gibi çiz
+    G4VisAttributes* tankVis = new G4VisAttributes(G4Colour(0.0, 1.0, 0.0)); 
+    tankVis->SetForceWireframe(true); 
     logicTank->SetVisAttributes(tankVis);
+
+    // =========================================================
+    // SİYAH DUVAR: DUVARE ÇARPAN FOTONU ANINDA YUT (SEKMEYİ ÖNLER)
+    // =========================================================
+    G4OpticalSurface* zifiriKaranlik = new G4OpticalSurface("ZifiriKaranlik");
+    zifiriKaranlik->SetType(dielectric_metal);
+    zifiriKaranlik->SetFinish(polished);
+    zifiriKaranlik->SetModel(unified);
+
+    G4double yansimaSifir[] = {0.0, 0.0};  
+    G4double yutmaYuzdeYuz[] = {1.0, 1.0}; 
+
+    G4MaterialPropertiesTable* mptSiyah = new G4MaterialPropertiesTable();
+    mptSiyah->AddProperty("REFLECTIVITY", photonEnergy, yansimaSifir, 2);
+    mptSiyah->AddProperty("EFFICIENCY", photonEnergy, yutmaYuzdeYuz, 2);
+    zifiriKaranlik->SetMaterialPropertiesTable(mptSiyah);
+
+    new G4LogicalSkinSurface("TankSiyahKaplama", logicTank, zifiriKaranlik);
+    // =========================================================
 
     return physWorld;
 }
